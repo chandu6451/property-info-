f72cc03db88c4da41a2c145467fa729961e0863d
chandu6451 <43878555+chandu6451@users.noreply.github.com>
2018-10-06 15:11:50 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace PropertyInfoDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly myAssembly = Assembly.LoadFrom("ReflectionLibrary.dll");
+
+            Type t = myAssembly.GetType("ReflectionLibrary.Employee");
+
+            PropertyInfo[] props = t.GetProperties();
+            Console.WriteLine("Number of Properties : " + props.Length);
+
+            foreach (PropertyInfo p in props)
+            {
+                Console.WriteLine("Property Name : " + p.Name);
+                Console.WriteLine("Can Read : " + p.CanRead);
+                Console.WriteLine("Can Write : " + p.CanWrite);
+                Console.WriteLine("Property Type : " + p.PropertyType);
+                Console.WriteLine();
+            }
+
+            object empObj = myAssembly.CreateInstance("ReflectionLibrary.Employee");
+            PropertyInfo id = t.GetProperty("EmpID");
+            id.SetValue(empObj, 101);
+            Console.WriteLine(id.GetValue(empObj));
+
+            Console.ReadKey();
+        }
+    }
+}
